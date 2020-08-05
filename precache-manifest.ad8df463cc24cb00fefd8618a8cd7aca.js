61b26dbe63bdcbb54029550d9fb99d90fa05ae0b
kmaq07 <kqusalh484@gmail.com>
2020-08-05 14:49:22 +0400

    Updates

---
+++ precache-manifest.ad8df463cc24cb00fefd8618a8cd7aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae2b880662af1321f55d8f58035673e",
+    "url": "/tailwind-app/index.html"
+  },
+  {
+    "revision": "f6aec3f5c77bdc08a142",
+    "url": "/tailwind-app/static/css/main.f96ddc9c.chunk.css"
+  },
+  {
+    "revision": "96084002d0e6b90416ce",
+    "url": "/tailwind-app/static/js/2.13b7cb7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailwind-app/static/js/2.13b7cb7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6aec3f5c77bdc08a142",
+    "url": "/tailwind-app/static/js/main.f0ad6917.chunk.js"
+  },
+  {
+    "revision": "49c7d328761b84b9242e",
+    "url": "/tailwind-app/static/js/runtime-main.e01b3f9d.js"
+  }
+]);
